4676617d6c09cff418c4fdff6a5f19187d29c7e3
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-28 17:01:31 -0500

    feat: completion of syntax exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,37 @@ public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
         System.out.print("Hello,");
-        System.out.print(" World!");
+        System.out.println(" World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
     }
 }
