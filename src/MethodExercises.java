d738641ec194eb3d5a18f802f74bb8f397bd156c
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-02 11:19:02 -0500

    Feat:established methods

---
+++ src/MethodExercises.java
@@ -0,0 +1,16 @@
+public class MethodExercises {
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+    public static int subtraction(int x, int y) {
+        return x - y;
+    }
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+    public static int division(int x, int y) {
+        return x / y;
+    }
+
+//    int five = addition(2,3);
+}
