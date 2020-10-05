c34049855c0461cebbba7734296eedc706d43669
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-05 13:49:27 -0500

    Feat: initial commit of input validation exercise

---
+++ src/util/Input.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    String getString(){
+        String userInput = scanner.nextLine();
+        String output = String.format("The userInput was : %s", userInput);
+        return output;
+    }
+
+    boolean yesNo(){
+        String userInput = scanner.nextLine();
+        if (userInput.equalsIgnoreCase("Y") || userInput.equalsIgnoreCase("yes")){
+            return true;
+        }
+        return false;
+    }
+}
