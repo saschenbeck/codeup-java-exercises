c99b83d106681be236a88372e436f363f47b07c5
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-07 08:59:14 -0500

    feat: Completion of Exercise 1

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -7,20 +7,34 @@ public class ArraysExercises {
         System.out.println(Arrays.toString(numbers));
         //You need Arrays.toString, otherwise it will print out the id of the array rather than the content
 
-//        Person people[] = new Person[3];
-//        System.out.println(Arrays.toString(people));
-//        for (Person person : people){
-//            System.out.println(person);
-//        }
+        //Person people[] = new Person[3];
+        //System.out.println(Arrays.toString(people));
+        //for (Person person : people){
+        //    System.out.println(person);
+        //}
 
         Person bob = new Person("Bob");
         Person billy = new Person("Billy");
         Person benjamin = new Person("Benjamin");
 
-        Person people[] = {bob,billy,benjamin};
-        System.out.println(Arrays.toString(people));
-        for (Person person : people){
-            System.out.println(person.getName());
+        Person[] people = new Person[3];
+        people[0] = bob;
+        people[1] = billy;
+        people[2] = benjamin;
+
+        //for (Person person : people){
+        //  System.out.println(person.getName());
+        //}
+        addPerson(people, new Person("brandon"));
+
+
+
+    }
+    public static void addPerson(Person[] people, Person personToAdd){
+        Person[] copyArray = Arrays.copyOf(people, people.length+1);
+        copyArray[copyArray.length-1] = personToAdd;
+        for(Person newPerson: copyArray){
+            System.out.println(newPerson.getName());
         }
     }
 }
