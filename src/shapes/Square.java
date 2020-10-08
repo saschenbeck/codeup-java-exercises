2bd002789533f38bb7bdf62c7b2c55da3b7e6b65
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-08 10:19:25 -0500

    Feat: Override of getArea and getPerimeter for Square; used sout to establish that override occured

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle {
+    private double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
+
+
+    public double getPerimeter(){
+//        System.out.println("square-perimeter");
+        return side * 4;
+    }
+
+    public double getArea(){
+//        System.out.println("square-area");
+        return Math.pow(side, 2);
     }
 }
