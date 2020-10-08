9bda80857d113f694080d1adadc6ae96fa5ab9d3
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-08 16:21:10 -0500

    Feat: Creation of abstract Quadrilateral; creation of abstract Shape; creation of implementation Measurable; extended Shape and implemented Measurable to Quadrilateral; extended Quadrilateral to Rectangle and Square; refactored Rectangle and Square to accept implementation and super class

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,9 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("box1.getArea() = " + box1.getArea());
-        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
-
-        Rectangle box2 = new Square(5);
-        System.out.println("box2.getArea() = " + box2.getArea());
-        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
+        Measurable myShape = new Square(4);
+        System.out.println();
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
     }
 }
